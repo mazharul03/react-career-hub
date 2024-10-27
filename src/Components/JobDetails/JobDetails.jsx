f61bd19b190fd3d752d00e082be80ba0a236671d
Mazharul Islam <mazharul3islam0@gmail.com>
2024-10-27 19:51:36 +0600

    Add Icons and handle Show All jobs

--- src/Components/JobDetails/JobDetails.jsx
+++ src/Components/JobDetails/JobDetails.jsx
@@ -1,5 +1,9 @@
+import { useLoaderData, useParams } from "react-router-dom";
 
 const JobDetails = () => {
+    const jobs = useLoaderData();
+    const {id}= useParams();
+    console.log(id,jobs);
     return (
         <div>
             <h2>Show Details of :</h2>
--- src/main.jsx
+++ src/main.jsx
@@ -27,7 +27,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/job/:id',
-        element:<JobDetails></JobDetails>
+        element:<JobDetails></JobDetails>,
+        loader:()=>fetch('../jobs.json')
       }
     ]
   },
